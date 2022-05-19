725f7c49d872cb555d0cf8d39b0e3dc47ed491fe
lucasRosssi <lucasrm2305@gmail.com>
2022-05-19 12:45:43 -0300

    added new routes

--- src/routes/establishments.routes.ts
+++ src/routes/establishments.routes.ts
@@ -1,5 +1,25 @@
 import { Router } from 'express';
+import { createEstablishmentController } from '../services/createEstablishment';
+import { createMenuController } from '../services/createMenu';
+import { listEstablishmentsController } from '../services/listEstablishments';
+import { listMenuController } from '../services/listMenu';
 
 const establishmentsRoutes = Router();
+
+establishmentsRoutes.post('/', (request, response) => {
+	return createEstablishmentController.handle(request, response);
+});
+
+establishmentsRoutes.get('/', (request, response) => {
+	return listEstablishmentsController.handle(request, response);
+});
+
+establishmentsRoutes.post('/:establishment_id/menu', (request, response) => {
+	return createMenuController.handle(request, response);
+});
+
+establishmentsRoutes.get('/:establishment_id/menu', (request, response) => {
+	return listMenuController.handle(request, response);
+});
 
 export { establishmentsRoutes };
