b3bd11ec59678c2b6eae93037c85967cc7027b84
lucasRosssi <lucasrm2305@gmail.com>
2022-05-21 12:55:08 -0300

    added dishes routes

--- src/routes/establishments.routes.ts
+++ src/routes/establishments.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+
+import { createDishController } from '../services/createDish';
 import { createEstablishmentController } from '../services/createEstablishment';
 import { createMenuController } from '../services/createMenu';
+import { listDishController } from '../services/listDish';
 import { listEstablishmentsController } from '../services/listEstablishments';
 import { listMenuController } from '../services/listMenu';
 
@@ -21,5 +24,13 @@ establishmentsRoutes.post('/:establishment_id/menu', (request, response) => {
 establishmentsRoutes.get('/:establishment_id/menu', (request, response) => {
 	return listMenuController.handle(request, response);
 });
+
+establishmentsRoutes.post('/:establishment_id/menu/:menu_id/dishes', (request, response) => {
+	return createDishController.handle(request, response);
+})
+
+establishmentsRoutes.get('/:establishment_id/menu/:menu_id/dishes', (request, response) => {
+	return listDishController.handle(request, response);
+})
 
 export { establishmentsRoutes };
