e8116b7061d40d741c75bde766d8629a5eac3c33
lucasRosssi <lucasrm2305@gmail.com>
2022-05-21 13:57:06 -0300

    create new delete route

--- src/routes/establishments.routes.ts
+++ src/routes/establishments.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { createDishController } from '../services/createDish';
 import { createEstablishmentController } from '../services/createEstablishment';
 import { createMenuController } from '../services/createMenu';
+import { deleteMenuController } from '../services/deleteMenu';
 import { listDishController } from '../services/listDish';
 import { listEstablishmentsController } from '../services/listEstablishments';
 import { listMenuController } from '../services/listMenu';
@@ -25,12 +26,16 @@ establishmentsRoutes.get('/:establishment_id/menu', (request, response) => {
 	return listMenuController.handle(request, response);
 });
 
-establishmentsRoutes.post('/:establishment_id/menu/:menu_id/dishes', (request, response) => {
+establishmentsRoutes.post('/:establishment_id/menu/:category/dishes', (request, response) => {
 	return createDishController.handle(request, response);
 })
 
-establishmentsRoutes.get('/:establishment_id/menu/:menu_id/dishes', (request, response) => {
+establishmentsRoutes.get('/:establishment_id/menu/:category/dishes', (request, response) => {
 	return listDishController.handle(request, response);
+})
+
+establishmentsRoutes.delete('/:establishment_id/menu', (request, response) => {
+	return deleteMenuController.handle(request, response);
 })
 
 export { establishmentsRoutes };
